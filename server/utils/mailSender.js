42fa4cc77edcc9ab0b34f6f2ed69dff1eb35e416
iamuv26 <114980708+iamuv26@users.noreply.github.com>
2025-11-27 18:53:11 +0530

    Fix SMTP timeout: Switch to Port 465 (SSL)

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -4,12 +4,12 @@ const mailSender = async (email, title, body) => {
   try {
     let transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
-      port: 587,
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
       },
-      secure: false,
     })
 
     let info = await transporter.sendMail({
